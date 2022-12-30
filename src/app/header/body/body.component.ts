efa349e34aa954beabf6510f531ced64435629c6
Saavedra98 <saavedraerazoc@gmail.com>
2022-12-30 10:01:05 -0500

    fin hora clase

--- src/app/header/body/body.component.ts
+++ src/app/header/body/body.component.ts
@@ -9,14 +9,20 @@ export class BodyComponent {
   usuario = 'Angular'
   password = "123456"
 
+  aceptado = false;
+  rechazado = false;
+
   items = ['Home', 'Support', 'Contact']
 
   verificar(usuarioInput: String, passwordInput:String){
 
     if (usuarioInput == this.usuario && passwordInput == this.password) {
-      alert("bienvenido")
+      (this.aceptado = true);
+      this.rechazado=false;
     }else{
-      alert("rechazado")
+      // alert("rechazado")
+      (this.rechazado = true);
+      this.aceptado=false;
     }
 
   }
